74dd634e5f9b099fd8c621497a999cff110ce902
Carlos Valter dos Santos Ferreira <carlosvalter@hotmail.com>
2020-06-04 16:58:56 -0300

    Desafio 5 resolvido

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,43 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = this.transactions.reduce(
+      (transactionAcumulador: Balance, transactionAtual: Transaction) => {
+        switch (transactionAtual.type) {
+          case 'income':
+            transactionAcumulador.income += transactionAtual.value;
+            break;
+          case 'outcome':
+            transactionAcumulador.outcome += transactionAtual.value;
+            break;
+
+          default:
+            break;
+        }
+        return transactionAcumulador;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    balance.total = balance.income - balance.outcome;
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
